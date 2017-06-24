ff8a7aab2a5a6c082e154b48688a2860c6731c96
Dorian Morgan <doorain@gmail.com>
2017-06-24 00:55:10 -0600

    init commit set up

---
+++ server.js
@@ -0,0 +1,10 @@
+var Twit = require('twit');
+var config = require('./config.json');
+
+var T = new Twit({
+  consumer_key: config.consumerKey,
+  consumer_secret: config.consumer_secret,
+  access_token: config.access_token,
+  access_token_secret: config.access_token_secret,
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+})
