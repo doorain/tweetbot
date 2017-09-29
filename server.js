31c04652dd6d290b4772331ef78fda51933c2f00
Dorian Morgan <dmorgan@pickyourplum.com>
2017-09-28 18:42:29 -0600

    for loop though data

--- server.js
+++ server.js
@@ -21,5 +21,9 @@ var params = {
 T.get('search/tweets', params, gotData);
 
 function gotData(err, data, response) {
-  console.log(data)
+  var tweets = data.statuses;
+  for (var i = 0; i < tweets.length; i++){
+      console.log(tweets[i].text)
+  }
+
 };
