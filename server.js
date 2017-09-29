88a4e00456bc2d25fe6064b8f7c3501f58b7f198
Dorian Morgan <dmorgan@pickyourplum.com>
2017-09-29 13:16:36 -0600

    wrap operators in functions

--- server.js
+++ server.js
@@ -12,33 +12,37 @@ var T = new Twit({
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 });
 
+function search(){
+  var params = {
+    q: 'Taylor Swift',
+    count: '20'
+  }
 
-// var params = {
-//   q: 'Taylor Swift',
-//   count: '20'
-// }
-//
-// T.get('search/tweets', params, gotData);
-//
-// function gotData(err, data, response) {
-//   var tweets = data.statuses;
-//   for (var i = 0; i < tweets.length; i++){
-//       console.log(tweets[i].text)
-//   }
-//
-// };
-var tweet = {
-  status: '#node.js from Node.js'
+  T.get('search/tweets', params, gotData);
+
+  function gotData(err, data, response){
+    var tweets = data.statuses;
+    for (var i = 0; i < tweets.length; i++){
+      console.log(tweets[i].text)
+    }
+  };
 }
 
-T.post('statuses/update', tweet, tweeted);
+function tweetIt(){
+  var tweet = {
+    status: '#node.js from Node.js'
+  }
 
-function tweeted(err, data, response) {
-   if (err){
-     console.log("Error");
-   }
-   else (console.log("Tweet Posted"));
+  T.post('statuses/update', tweet, tweeted);
+
+  function tweeted(err, data, response) {
+    if (err){
+      console.log("Error");
+    }
+    else (console.log("Tweet Posted"));
+  }
 }
+
 
 
 // T.get('followers/ids', { screen_name: 'doorainm' },  function (err, data, response) {
